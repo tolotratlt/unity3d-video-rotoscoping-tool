c0cbdab5a7ec4b311775c70769469c64612df801
tlt <tolotra.tlt@gmail.com>
2024-06-02 16:50:35 +0300

    Update Video Controller to execute in editor without entering in play mode

--- Assets/RotoscopyTool/Scripts/VideoController.cs
+++ Assets/RotoscopyTool/Scripts/VideoController.cs
@@ -6,6 +6,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+    [ExecuteInEditMode]
     [DisallowMultipleComponent]
     [RequireComponent(typeof(VideoPlayer))]
     public class VideoController : MonoBehaviour
